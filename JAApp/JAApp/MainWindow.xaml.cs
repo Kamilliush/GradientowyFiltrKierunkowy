3352e1d39276226b8118affc04a52c70a3e64967
Kamilliush <kjakut03@gmail.com>
2024-10-28 13:22:49 +0100

    Szkielet projektu

---
+++ JAApp/JAApp/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JAApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("/../../../../bin/JADll.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int MyProc1(int a, int b);
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            int a = 2;
+            int b = 2;
+
+            Debug.WriteLine(MyProc1(a, b));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 Kamil Jakut 
 Student 5 semestru na wydziale AEiI
 Grupa 11
-
-Należy dodać w katalogu głównym folder opencv
