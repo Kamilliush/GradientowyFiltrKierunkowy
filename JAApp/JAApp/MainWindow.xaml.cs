980903ab2d255cef641d1b5813eb015a89b49c0e
Kamilliush <kjakut03@gmail.com>
2024-11-17 18:12:04 +0100

    Gui

--- JAApp/JAApp/MainWindow.xaml.cs
+++ JAApp/JAApp/MainWindow.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+
 
 namespace JAApp
 {
@@ -19,15 +21,83 @@ namespace JAApp
     public partial class MainWindow : Window
     {
         [DllImport("/../../../../bin/JADll.dll", CallingConvention = CallingConvention.StdCall)]
-        public static extern int MyProc1(int a, int b);
+        public static extern int MyProc1(int a, int b);        
+        
+        [DllImport("/../../../../x64/Debug/cppDll.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int Add(int a, int b);
+
+        private BitmapSource loadedBitmap;
         public MainWindow()
         {
             InitializeComponent();
+            
 
             int a = 2;
             int b = 2;
 
             Debug.WriteLine(MyProc1(a, b));
+            Debug.WriteLine(Add(a, b));
+        }
+        private void ImageDragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (files != null && IsValidImageFile(files[0]))
+                {
+                    e.Effects = DragDropEffects.Copy;
+                }
+                else
+                {
+                    e.Effects = DragDropEffects.None;
+                }
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void ImageDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (files != null && IsValidImageFile(files[0]))
+                {
+                    SetImage(files[0]);
+                }
+                else
+                {
+                    Console.WriteLine("Nieprawidłowy plik obrazu.");
+                }
+            }
+        }
+        private void SetImage(string filePath)
+        {
+            try
+            {
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.UriSource = new Uri(filePath, UriKind.Absolute);
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                FormatConvertedBitmap rgbBitmap = new FormatConvertedBitmap(bitmapImage, PixelFormats.Rgb24, null, 0);
+                loadedBitmap = rgbBitmap;
+
+                image.Source = loadedBitmap;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Błąd podczas ładowania obrazu: {ex.Message}");
+            }
+        }
+
+        private bool IsValidImageFile(string filePath)
+        {
+            string extension = System.IO.Path.GetExtension(filePath)?.ToLower();
+            return extension == ".png" || extension == ".jpg" || extension == ".jpeg" || extension == ".bmp";
         }
     }
 }
